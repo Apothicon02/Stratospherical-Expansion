c76e2ead4af44d6c35db636e1d854592e31a65c7
Apothicon02 <79332745+Apothicon02@users.noreply.github.com>
2023-02-07 19:34:57 -0500

    -Initial release.

---
+++ src/main/java/com/Apothic0n/StratosphericalExpansion/StratosphericalExpansion.java
@@ -0,0 +1,27 @@
+package com.Apothic0n.StratosphericalExpansion;
+
+import com.Apothic0n.StratosphericalExpansion.api.biome.StratoSurfaceRuleData;
+import com.Apothic0n.StratosphericalExpansion.api.biome.features.StratoFeatureRegistry;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import terrablender.api.SurfaceRuleManager;
+
+@Mod(StratosphericalExpansion.MODID)
+public class StratosphericalExpansion {
+    public static final String MODID = "strato";
+
+    public StratosphericalExpansion() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+
+        StratoFeatureRegistry.register(eventBus);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            SurfaceRuleManager.setDefaultSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, StratoSurfaceRuleData.makeRules());
+        });
+    }
+}
---
+++ src/main/java/com/Apothic0n/StratosphericalExpansion/api/biome/features/StratoFeatureRegistry.java
@@ -0,0 +1,21 @@
+package com.Apothic0n.StratosphericalExpansion.api.biome.features;
+
+import com.Apothic0n.StartosphericalExpansion.api.biome.features.configurations.RockConfiguration;
+import com.Apothic0n.StartosphericalExpansion.api.biome.features.types.SpheroidRockFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public abstract class StratoFeatureRegistry {
+    public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, com.Apothic0n.StratosphericalExpansion.StratosphericalExpansion.MODID);
+
+    public static final RegistryObject<Feature<RockConfiguration>> SPHEROID_ROCK = FEATURES.register("spheroid_rock", () ->
+            new SpheroidRockFeature(RockConfiguration.CODEC));
+
+    public static void register(IEventBus eventBus) {
+        FEATURES.register(eventBus);
+    }
+}
+
