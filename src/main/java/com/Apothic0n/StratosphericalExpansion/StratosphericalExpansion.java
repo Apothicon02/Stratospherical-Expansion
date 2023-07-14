0da92e7c649566e903182bf809c14c1c3a8b27aa
Apothic0n <tylerzm3@gmail.com>
2023-07-14 13:12:32 -0400

    -Updated to 0.3.0. -There are now tons of cave features. -Ancient cities can now generate. -The surface builder is now greatly improved. -Recreated the Frozen River biome. -Recreated the Snowy Beach biome. -Recreated the Bamboo Jungle biome. -Recreated the Savanna Plateau biome. -Recreated the Windswept Savanna biome. -Recreated the Windswept Hills biome. -Recreated the Windswept Gravelly Hills biome. -Recreated the Frozen Ocean biome. -Recreated the Deep Frozen Ocean biome. -Recreated the Cold Ocean biome. -Recreated the Deep Cold Ocean biome. -Recreated the Ocean biome. -Recreated the Deep Ocean biome. -Recreated the Lukewarm Ocean biome. -Recreated the Warm Ocean biome. -Cave vines now generate less desnsely the lower in altitude you go. Note: Eventhough the ocean biomes have been recreated, they still shouldn't generate. The purpose of recreating them is so if something causes them to generate for any reason, such as an incompatability with another mod, they wont look so barren and out of place.

--- src/main/java/com/Apothic0n/StratosphericalExpansion/StratosphericalExpansion.java
+++ src/main/java/com/Apothic0n/StratosphericalExpansion/StratosphericalExpansion.java
@@ -2,8 +2,10 @@ package com.Apothic0n.StratosphericalExpansion;
 
 import com.Apothic0n.StratosphericalExpansion.api.biome.StratoSurfaceRuleData;
 import com.Apothic0n.StratosphericalExpansion.api.biome.features.StratoFeatureRegistry;
+import com.Apothic0n.StratosphericalExpansion.core.objects.StratoBlocks;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import terrablender.api.SurfaceRuleManager;
@@ -15,13 +17,19 @@ public class StratosphericalExpansion {
     public StratosphericalExpansion() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
 
         StratoFeatureRegistry.register(eventBus);
+        StratoBlocks.BLOCKS.register(eventBus);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             SurfaceRuleManager.setDefaultSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, StratoSurfaceRuleData.makeRules());
         });
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        StratoBlocks.fixBlockRenderLayers();
     }
 }
